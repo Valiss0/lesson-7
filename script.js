d7d34c7e6db3f67737dbc9f82d539a5c3dc754a3
Valiss0 <92210508+Valiss0@users.noreply.github.com>
2021-12-04 00:07:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonElement = document.getElementById('button');
+let inputElement = document.getElementById('text');
+
+
+buttonElement.addEventListener('click', function(event) {
+    alert(text.value);
+});
